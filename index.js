59602c7ae02e73eafb2e3c5bd369756415ff79ff
tuoiuuu676 <katemuirxopun0suy@hotmail.com>
2024-05-12 07:52:38 +0700

    update project

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-function wordBreak(s, wordDict) {
-  const wordSet = new Set(wordDict);
-  const dp = new Array(s.length + 1).fill(false);
-  dp[0] = true;
-  for (let end = 1; end <= s.length; end++) {
-    for (let start = 0; start < end; start++) {
-      if (dp[start] && wordSet.has(s.substring(start, end))) {
-        dp[end] = true;
-        break;
-      }
-    }
-  }
-  return dp[s.length];
-}
+const remove = (arr, func) =>
+  Array.isArray(arr)
+    ? arr.filter(func).reduce((acc, val) => {
+        arr.splice(arr.indexOf(val), 1);
+        return acc.concat(val);
+      }, [])
+    : [];
